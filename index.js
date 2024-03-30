2d568e9e4224d67b1c039861347c984db41ba7ca
shivam <mandjoai123@gmail.com>
2024-03-30 09:44:27 +0530

    .

--- index.js
+++ index.js
@@ -1,84 +1,45 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
+import router from './routes/useRoutes.js'
+import mongoose from "mongoose";
+import cookieParser from 'cookie-parser'
+import formData from 'express-form-data'
+async function connectDb(DATABASE_URL) {
+    try {
+        const DB_options = {
+            dbname: "shivam"
+        };
+        await mongoose.connect(DATABASE_URL, DB_options);
+        console.log("Database Connected........");
+    } catch (error) {
+        console.log(error);
+    }
 
-const mongoString = "mongodb+srv://mehulkumar:mehul%402401@cluster0.8qvjaia.mongodb.net/LoginR";
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+}
 
 const app = express()
+const port=process.env.PORT
+const DATABASE_URL=process.env.DATABASE_URL
+
+connectDb(DATABASE_URL)
+
+const corsOptions = {
+    credentials: true,
+    origin: 'http://localhost:5173'
+  };
+  
+
+
+
+app.use(cors(corsOptions));
 app.use(express.json())
-app.use(express.urlencoded())
-app.use(cors({
-    origin:"https://localhost:3000",
-    credentials:true,
-}))
+app.use(cookieParser())
+app.use(formData.parse());
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
-
-const User = new mongoose.model("User", userSchema)
-
-app.post("/login", async (req, res) => {
-    const { email, password } = req.body
-    try {
-        const data = await User.findOne({ email: email })
-        if (data) {
-            if (password === data.password) {
-                res.status(200).json({ message: "Login Sucessfully" })
-                alert("Login Sucessfully")
-            } else {
-                res.status(400).json({ message: "Invalid password" })
-                alret("Invalid password")
-            }
-        } else {
-            res.status(400).json({ message: "User didn't Register" })
-            alret("User didn't Register")
-        }
-    }catch(err){
-        res.status(400).json({ message: err })
-    }
-})
-
-
-app.post("/register", async (req, res) => {
-    const { name, email, password } = req.body
-    try {
-        const data = await User.findOne({ email: email })
-        if (data) {
-            res.status(400).json({ message: "user already exsist" })
-            alret("user already exsist")
-        }
-        const user = new User({
-            name,
-            email,
-            password
-        })
-        await user.save()
-        res.status(200).json({ message: "data saved" })
-
-    }
-    catch (err) {
-        res.status(400).json({ message: err })
-    }
-})
-
-
-
-
-
-app.listen(5454, () => {
-    console.log("Port is Running on 5454.")
+app.use("/api/users",router)
+app.listen(port,()=>{
+    console.log(`listening port on ${port}`)
 })
